85412e43253d20766b8aafaf297564683d3cfafa
akorda <akorda@gmail.com>
2016-12-19 20:22:14 +0200

    missing file added

---
+++ src/Mp3Effects/UI/EmptyProgressNotifier.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Mp3Effects.UI
+{
+    public class EmptyProgressNotifier : IProgressNotifier
+    {
+        public bool Enabled { get; set; } = true;
+        
+        public void Initialize(int maxTicks, string message)
+        {        
+        }
+
+        public void UpdateMessage(string message)
+        {                    
+        }
+
+        public void Tick(string message = null)
+        {            
+        }
+    }
+}
